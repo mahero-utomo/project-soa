d19f0c25947bfd2e90c7dc4ef54dbf7e41a84567
Edwin Lo <edwin.lo.el1@gmail.com>
2021-05-06 15:58:12 +0700

    push pertama

---
+++ connection.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+var pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "soa_m7_senin"
+});
+
+const executeQueryWithParam = async (query, param) => {
+    return new Promise((resolve, reject) => {
+        pool.query(query, param, (err, rows, fields) => {
+            if (err) reject(err);
+            else resolve(rows);
+        })
+    })
+}
+
+module.exports= {
+    'executeQueryWithParam' : executeQueryWithParam,
+}
